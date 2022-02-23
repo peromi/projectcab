eb12e4b9f43882501b520fd333b4b763b0c4267a
maceteli <peteralagoa@gmail.com>
2022-02-23 23:53:44 +0000

    changes is everything

--- lib/utills/constants.dart
+++ lib/utills/constants.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled1/model/users.dart';
 
-String APIKEY = "AIzaSyBqve7CQbRBwQmbHWmaPIBCJZQXIFyNkTE";
-
 Color leave_green = Color(0xFF006B3D);
 Color dirty_green = Color(0xFF046A55);
 
